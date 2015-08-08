4526b554df707378d6394cfa4fd653a758f36198
costa <deepcell@gmail.com>
2015-08-09 03:38:48 +0700

    ..

--- combo_cidade.php
+++ combo_cidade.php
@@ -1,13 +1,13 @@
-<?php 
-echo "<select name='town' id='cidade'>"; 
-echo "<option value='0'>[selecione]</option>"; 
+<?php
+echo "<select name='town' id='cidade'>";
+echo "<option value='0'>[selecione]</option>";
 
 include("includes/config/config.php");
 
 $sql = "SELECT tb_cidades.nome, tb_cidades.id FROM tb_cidades INNER JOIN tb_estados ON tb_cidades.uf=tb_estados.uf WHERE tb_cidades.uf='".$_GET["uf"]."'";
-$resultado = mysql_query($sql); 
+$resultado = mysql_query($sql);
 while ($linha = mysql_fetch_array($resultado))
-{ 
+{
     if ( $_GET["cid"] == $linha["id"] ) 
     {
     	$sel = "selected";
@@ -16,7 +16,7 @@ while ($linha = mysql_fetch_array($resultado))
     {
     	$sel = "";
     }
-	echo "<option value='$linha[id]' $sel>$linha[nome]</option>";
-} 
-echo "</select>"; 
+    echo "<option value='$linha[id]' $sel>$linha[nome]</option>";
+}
+echo "</select>";
 ?>
