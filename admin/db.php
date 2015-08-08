5ab8d3d3d8253454fcf4ac3d3c676f0b9aa2011c
Fernando Costa <deepcell@gmail.com>
2015-08-08 22:11:30 +0700

    Update db.php
    ..

--- admin/db.php
+++ admin/db.php
@@ -1,9 +1,9 @@
 <?php
 $mysql_hostname  = "localhost";
-$mysql_user 	   = "delivery";
+$mysql_user      = "delivery";
 $mysql_password  = "delivery";
 $mysql_database  = "delivery";
-$prefix 		     = "";
+$prefix          = "";
 
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("ERROR: DB CONNECTION");
 mysql_select_db($mysql_database, $bd) or die("ERROR: DB SELECT");
