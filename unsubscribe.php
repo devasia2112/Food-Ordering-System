ca04db1eeeb86c9df119a2c8d5df6ab2808baad3
costa <deepcell@gmail.com>
2015-08-09 02:28:36 +0700

    this will be only a flag that the user do not want to be subscribed to the news- letter.

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,1 +1,7 @@
-create unsubscribe
+create unsubscribe file -  newsletter system can be integrated easily using and open source software as PHPList.
+
+PENDING
+	create a subscribe option in the customer area.
+	create a column data-type boolean in customer's table.
+
+see TODO for more.
