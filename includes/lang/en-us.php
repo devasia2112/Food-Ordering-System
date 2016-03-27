e258e1b05323ce1fc98ca5ed96fb180c6103e361
costa <deepcell@gmail.com>
2016-03-27 22:46:44 +0700

    New translation text was added. It is a working in progress.

--- includes/lang/en-us.php
+++ includes/lang/en-us.php
@@ -1,5 +1,6 @@
 <?php
-define ( "TITLE_INDEX", "Thai Food Delivery Services - Lunch & Dinner Delivery Brazil" );
+#----------------------------------frontend-------------------------------------#
+define ( "TITLE_INDEX", "Food Delivery Services - Lunch & Dinner Delivery" );
 
 define ( "BTN_START_CHAT", "Start Chat" );
 
@@ -36,7 +37,6 @@ define ( "LBL_TOTAL", "Total" );
 define ( "LBL_UPDATE", "Update" );
 define ( "LBL_CHECKOUT", "Checkout" );
 define ( "LBL_CHECKOUT_PAYPAL", "Checkout with PayPal" );
-#-----------------------cart------------------------
 
 
 
@@ -63,7 +63,6 @@ define ( "LBL_TAKEAWAY", "Take Away " );
 define ( "LBL_ZIPCODE", "Zipcode " );
 define ( "LBL_ZIPCODE_ENTER_HERE", "Enter your zipcode " );
 
-#-------------MENU----------------------------------
 
 
 
@@ -85,7 +84,6 @@ define( "LBL_UPDATE_ADDRESS", "Update Address" );
 define( "LBL_DELIVERY_ORDER", "Delivery Address" );
 define( "LBL_PAYMENT_METHOD", "Payment Method" );
 
-#---------------------checkout------------------------------
 
 
 #---------------------checkout-process----------------------
@@ -96,7 +94,6 @@ define( "LBL_CUSTOMER_THANKYOU", "Thank you for order with us." );
 define( "LBL_CUSTOMER_CONFIRMATION", "This is only a confirmation of your order." );
 
 
-#---------------------checkout-process----------------------
 
 
 #------------------customer-register-----------------------
@@ -165,12 +162,11 @@ define ( "MSG_CUSTOMER_INSERT_PHONE_1", "The field PHONE 1 is required.<br>" );
 define ( "MSG_CUSTOMER_INSERT_TERMS", "Accept TERMS & CONDITIONS is required.<br>" );
 define ( "LBL_UPDATE_PASSWORD", "Update Password" );
 define ( "LBL_EMAIL_EXIST", "This email has already been registered in our database." );
-#------------------customer-register-----------------------
 
 
 #------------login--------------
 define( "H2_CUSTOMER_LOGIN", "Log In" );
-define( "LOGIN_SOCIAL_NETWORK", "Do you already have an account on one of these sites? Click the logo to log in with it here:" ); 
+define( "LOGIN_SOCIAL_NETWORK", "Do you already have an account on one of these sites? Click the logo to log in with it here:" );
 define( "TXT_H2_OPENID", "
 Why OpenID? <br />
 It’s a single username and password that allows you to log in to any OpenID-enabled site. <br />
@@ -189,20 +185,19 @@ define( "LNK_CUSTOMER_LOGIN_SIGIN", "Log In" );
 define( "TXT_H2_REGISTER_NEW_CUSTOMER", "Don't have an account yet?" );
 define( "LBL_OR", "or" );
 
-#------------login--------------
 
 
 #------------recovery account popup-------------------------
 define( "H2_CUSTOMER_LOGIN_RECOVERY", "Account Recovery" );
 define( "LBL_BTN_RECOVERY", "GO" );
-#------------recovery account popup-------------------------
+
 
 #------------map------------------------
 define( "LBL_FIND_US", "Find Us" );
 define( "MSG_DELIVERY_AREA_PRICE", "Click the area you want delivery, to see the value." );
 define( "MSG_DELIVERY_AREA_SEE_GOOGLE", "Click here to see it in a big " );
 
-#------------map------------------------
+
 
 
 #------------contact------------------------
@@ -214,17 +209,154 @@ define( "LBL_ORDER_EASY", "3 EASY STEPS!" );
 define( "LBL_STEP_1", "Enter your zipcode" );
 define( "LBL_STEP_2", "Select your food" );
 define( "LBL_STEP_3", "Order online and enjoy" );
-
 define( "LBL_HOWTO_STEP_1", "Create or access your account. Make sure your ZIP code already registered in your client area." );
 define( "LBL_HOWTO_STEP_2", "Click \"Menu\" on the top bar and select one or more courses <br> in our catalog. Schedule the day to take away or delivery." );
 define( "LBL_HOWTO_STEP_3", "Finalize the purchase and select the payment via PayPal <br> (All major cards) or pay on delivery." );
 
-#------------contact------------------------
 
 
 
 #------------footer-----------------
 define( "LBL_SAFE_ENVIRONMENT", "SAFE ENVIRONMENT 256 BITS" );
 
-#------------footer-----------------
+#----------------------------------frontend-------------------------------------#
 
+
+
+
+# ***related only to the admin area
+#----------------------------------backend--------------------------------------#
+
+#---main-menu-----------------
+define("MENU_TITLE_1", "Settings");
+define("MENU_TITLE_1_1", "FrontEnd::Logo");
+define("MENU_TITLE_1_2", "FrontEnd::Slider - Images");
+define("MENU_TITLE_1_3", "FrontEnd::Howto Order");
+define("MENU_TITLE_1_4", "FrontEnd::Terms");
+define("MENU_TITLE_1_5", "FrontEnd::Privacy");
+define("MENU_TITLE_1_6", "FrontEnd::Map(Find Us)");
+define("MENU_TITLE_1_7", "Company Settings");
+define("MENU_TITLE_1_8", "Email Settings");
+
+define("MENU_TITLE_2", "Customized Menu");
+define("MENU_TITLE_2_1", "Category");
+define("MENU_TITLE_2_2", "Products");
+define("MENU_TITLE_2_3", "Coupons");
+define("MENU_TITLE_2_4", "Generate Menu (Public PDF)");
+define("MENU_TITLE_2_5", "Generate Menu (Customers)");
+
+define("MENU_TITLE_3", "Customers");
+define("MENU_TITLE_3_1", "Customer Registration");
+define("MENU_TITLE_3_2", "Customer Orders (Write Off)");
+define("MENU_TITLE_3_3", "Customer Orders (Paid)");
+
+define("MENU_TITLE_4", "Suppliers");
+define("MENU_TITLE_4_1", "Supplier Registration");
+define("MENU_TITLE_4_2", "Supplier Orders");
+define("MENU_TITLE_4_3", "Order History");
+define("MENU_TITLE_4_4", "Supplier Credit");
+
+define("MENU_TITLE_5", "Localization");
+  define("MENU_TITLE_5_a", "(rules in config file)");
+define("MENU_TITLE_5_1", "Language (Front/Back end)");
+define("MENU_TITLE_5_2", "Monetary Locale");
+define("MENU_TITLE_5_3", "Timezone");
+define("MENU_TITLE_5_4", "Fiscal Settings");
+
+define("MENU_TITLE_6", "Fiscal");
+  define("MENU_TITLE_6_a", "PT-BR (Only)");
+
+define("MENU_TITLE_7", "Reports");
+define("MENU_TITLE_7_1", "Customer Orders");
+define("MENU_TITLE_7_2", "Viewed Products");
+define("MENU_TITLE_7_3", "Top Selling Products");
+define("MENU_TITLE_7_4", "Receivables Bills");
+define("MENU_TITLE_7_5", "Daily Sales");
+define("MENU_TITLE_7_6", "Monthly Sales");
+define("MENU_TITLE_7_7", "Anually Sales");
+define("MENU_TITLE_7_8", "Payables Bills");
+define("MENU_TITLE_7_9", "Payables Bills (All)");
+define("MENU_TITLE_7_10", "Receivables & Payables Bills");
+
+define("MENU_TITLE_8", "Management Tools");
+define("MENU_TITLE_8_1", "Accounting (Revenue & Expense)");
+define("MENU_TITLE_8_2", "Stock Control");
+define("MENU_TITLE_8_3", "Factsheet or Datasheet");
+define("MENU_TITLE_8_4", "Sales (Simple Web POS)");
+define("MENU_TITLE_8_5", "Payment (Gateways)");
+define("MENU_TITLE_8_6", "Payment (CC)");
+define("MENU_TITLE_8_7", "Customer Service");
+define("MENU_TITLE_8_8", "Database Backup");
+define("MENU_TITLE_8_9", "Newsletter");
+
+define("MENU_TITLE_9", "Access");
+define("MENU_TITLE_9_1", "Logout");
+define("MENU_TITLE_9_2", "Logs");
+
+
+#---logo-upload--------------------
+define("LOGO_TITLE_1", "Upload Logotipo");
+define("LOGO_TITLE_1_1", "Upload your logo here, it will be used widely used in the system.");
+define("LOGO_TITLE_2", "View your logo here");
+
+
+#---slider-upload.php---------------
+define("SLIDER_TITLE_1", "Images Upload");
+define("SLIDER_TITLE_1_1", "Upload Images to the Slider (front page)");
+
+#---howto-order.php-----------------
+define("HOWTO_ORDER_TITLE_1", "Frontend  - Howto Order Page");
+define("HOWTO_ORDER_TITLE_1_1", "Here goes the information accessed in `Delivery/how-to-order` page.");
+
+
+#---company----------------------
+define("COMPANY_TITLE_1", "Company Settings");
+define("COMPANY_TITLE_2", "Company Registration");
+define("COMPANY_TITLE_2_1", "...");
+define("COMPANY_TITLE_3", "List Companies");
+define("COMPANY_TITLE_3_1", "...");
+define("COMPANY_TITLE_4", "Company registration");
+
+define("COMPANY_INSERT_1", "Address Information");
+define("COMPANY_INSERT_2", "Company Information");
+define("COMPANY_INSERT_3", "Contact Information");
+define("COMPANY_INSERT_4", "Another Information");
+
+define("COMPANY_INPUT_1", "Location");
+define("COMPANY_INPUT_2", "Street");
+define("COMPANY_INPUT_3", "Number");
+define("COMPANY_INPUT_4", "Complement");
+define("COMPANY_INPUT_5", "Suburb");
+define("COMPANY_INPUT_6", "ZIP Code");
+define("COMPANY_INPUT_7", "Document 1");
+define("COMPANY_INPUT_8", "Document 2");
+define("COMPANY_INPUT_9", "Document 3");
+define("COMPANY_INPUT_10", "Document 4");
+define("COMPANY_INPUT_11", "Document 5");
+define("COMPANY_INPUT_12", "Document 6");
+define("COMPANY_INPUT_13", "Document 7");
+define("COMPANY_INPUT_14", "Phone 1");
+define("COMPANY_INPUT_15", "Phone 2");
+define("COMPANY_INPUT_16", "Person in Charge");
+define("COMPANY_INPUT_17", "FAX");
+define("COMPANY_INPUT_18", "Email");
+define("COMPANY_INPUT_19", "Website");
+define("COMPANY_INPUT_20", "Facebook Page");
+define("COMPANY_INPUT_20_a", "e.g.: https://www.facebook.com/<b>YOUR_PROFILE</b>");
+define("COMPANY_INPUT_21", "Google Map");
+define("COMPANY_INPUT_21_a", "e.g.: https://maps.google.com/maps/ms?msa=0...&amp;ll=-00.010101,-00.010101&amp;spn=0.061188,0.164623&amp;z=13&amp;output=embed");
+
+define("COMPANY_INPUT_22", "Open Time");
+define("COMPANY_INPUT_23", "Close Time");
+define("COMPANY_INPUT_24", "Observation");
+define("COMPANY_INPUT_25", "Text Front-end");
+define("COMPANY_INPUT_26", "");
+define("COMPANY_INPUT_27", "");
+
+define("COMPANY_BUTTON_1", "Register Company Data");
+define("COMPANY_BUTTON_2", "Update Company Data");
+
+
+
+
+#----------------------------------backend--------------------------------------#
