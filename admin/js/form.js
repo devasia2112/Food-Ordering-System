0475eb9f7ef41eeb914ef6555a4d13acfbd59e48
costa <deepcell@gmail.com>
2016-06-02 18:54:23 +0700

    this script is not in use anymore, however I will keep it here as a sample.

--- admin/js/form.js
+++ admin/js/form.js
@@ -10,20 +10,22 @@ $(function() {
 		var price 		= $("input#price").val();
 		var coupom 		= $("input#coupom").val();
 		//var photo		= $("input#photo").val();
-		
+
 		// Builds the string with posted values
 		var dataString = 'category=' + category + '&code=' + code + '&name=' + name + '&desc=' + desc + '&spicy=' + spicy + '&chef=' + chef + '&size=' + size + '&price=' + price + '&coupom=' + coupom + '&photoimg=' + photoimg;
-		
+
 		$.ajax({
 			type: "POST",
-			url: "products.act.php",
+			url: "../model/products-insert.php",
 			data: dataString,
 			cache: false, // Do not cache the page
 			success: function(msg) {
+				alert("success");
 				$("#alert").html(msg); // Display messages (php echo)
 			},
 			// If there is an Ajax error or cannot connect to actions.php, display the error message
 			error: function(msg) {
+				alert("error");
 				document.getElementById("alert").innerHTML="Your request could not be processed.";
 				$("#alert").slideDown("fast");
 			}
